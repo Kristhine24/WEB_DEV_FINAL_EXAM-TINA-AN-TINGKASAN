9c4d8ee10819ee5d60055eea633dff1ebc5dbc39
Kristhine24 <96170827+Kristhine24@users.noreply.github.com>
2021-12-15 21:13:43 +0800

    Add files via upload

---
+++ animal_footer.php
@@ -0,0 +1,30 @@
+<footer style="display:flex;justify-content:center">
+    <h2> Kristhine Kaye S. Tina-an &  Vanessamae J. Tingkasan </h2>
+</footer>
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
+/* start */
+.parentCon{
+    display: flex;
+    flex-wrap: wrap-reverse;
+    flex-direction: column;
+}
+main{
+    padding-top: 80px;
+    margin-left: 265px;
+}
+/* end */
+/* sidebar */
+.content{
+  margin-right: 5px;
+  padding-top: 50px;
+  transition: 0.5s;
+}
+</style>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
